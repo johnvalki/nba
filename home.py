e93c1a22e2ed51d1a63e8b8334a310c5b6068cb5
johnvalki <johnvalki12@gmail.com>
2025-11-20 17:17:52 -0500

    something

--- home.py
+++ home.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 
-page1 = st.page("main1.py")
-page2 = st.page("main2.py")
+page1 = st.Page("main1.py")
+page2 = st.Page("main2.py")
 pages = [page1,page2]
 nav = st.navigation(pages)
 nav.run()
