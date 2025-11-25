854d6854089a3f2e4023ed0b8d32b2727e9d972a
johnvalki <johnvalki12@gmail.com>
2025-11-25 17:07:18 -0500

    s

--- home.py
+++ home.py
@@ -4,7 +4,7 @@ def newpage():
 st.set_page_config(page_title="Home")
 page1 = st.Page("pages/main1.py",title="something")
 page2 = st.Page("main2.py",title="NBA teams comparison")
-pages = [page1,page2]
+pages = [page1,page2,page3]
 page3 = st.Page(newpage,title="function page")
 nav = st.navigation(pages,position='top')
 nav.run()
