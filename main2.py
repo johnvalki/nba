e8f87d86bb2b5ec38019bdae2bfc56960ff4a2fe
johnvalki <johnvalki12@gmail.com>
2025-11-25 17:04:32 -0500

    e

--- main2.py
+++ main2.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd 
 from matplotlib import pyplot as plt
 st.set_page_config(layout="wide")
-def newpage():
-    st.header("this is a function")
 st.set_page_config(page_title="NBA teams comparison")
 data = pd.read_csv("nba.csv")
 teamnames = sorted(data["hometeamName"].unique())
