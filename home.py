b9813847f460e490e1fdca4149084f65c9eb6592
johnvalki <johnvalki12@gmail.com>
2025-11-25 16:43:31 -0500

    mes

--- home.py
+++ home.py
@@ -3,5 +3,5 @@ st.set_page_config(page_title="Home")
 page1 = st.Page("main1.py",title="nba teams comparison")
 page2 = st.Page("main2.py",title="NBA teams comparison")
 pages = [page1,page2]
-nav = st.navigation(pages)
+nav = st.navigation(pages,position='top')
 nav.run()
