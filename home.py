0aa651700d2f4e5b9f9f48fead10d1d9f7e9206a
johnvalki <johnvalki12@gmail.com>
2025-11-25 16:53:59 -0500

    s

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.set_page_config(page_title="Home")
-page1 = st.Page("main1.py",title="NBA teams comparison")
+page1 = st.Page("pages/main1.py",title="NBA teams comparison")
 page2 = st.Page("main2.py",title="something")
 pages = [page1,page2]
 nav = st.navigation(pages,position='top')
